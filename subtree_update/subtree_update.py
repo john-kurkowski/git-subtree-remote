2c2a19c413b9cb5f0f92f2a338194ef52cdac102
John Kurkowski <john.kurkowski@gmail.com>
2015-12-27 23:51:17 -0800

    Reduce main command cyclomatic complexity

--- subtree_update/subtree_update.py
+++ subtree_update/subtree_update.py
@@ -202,6 +202,16 @@ def print_up_to_date(remote):
     ))
 
 
+def subtree_pull(local_repo, remote, squash):
+    subtree_args = ['pull', remote.repo['git_url'], 'master']
+
+    subtree_kwargs = {'prefix': remote.subtree.prefix}
+    if squash:
+        subtree_kwargs['squash'] = True
+
+    return local_repo.git.subtree(*subtree_args, **subtree_kwargs)
+
+
 @click.command()
 @click.option(
     'is_all', '--all',
@@ -240,17 +250,10 @@ def subtree_update(is_all, is_dry_run, squash, prefixes):
     updating_label = 'Updating {} subtrees'.format(len(subtree_remotes))
     with click.progressbar(subtree_remotes, label=updating_label) as progressbar:
         for remote in progressbar:
-            if not remote.is_ahead:
+            if remote.is_ahead:
+                subtree_pull(local_repo, remote, squash)
+            else:
                 click.echo('') # Newline after surrounding progress bar
                 print_up_to_date(remote)
-                continue
-
-            subtree_args = ['pull', remote.repo['git_url'], 'master']
-
-            subtree_kwargs = {'prefix': remote.subtree.prefix}
-            if squash:
-                subtree_kwargs['squash'] = True
-
-            local_repo.git.subtree(*subtree_args, **subtree_kwargs)
 
     click.echo(local_repo.git.status())
